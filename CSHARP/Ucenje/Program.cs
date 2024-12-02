53e9f2eedb8fad7605996435404ee76b2533df52
Ante Janković <bhorv4t@gmail.com>
2024-12-02 17:51:29 +0100

    Upoznavanje s sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//komentar jedna linija 
+
+/*
+ * 
+ * komentar vise linija 
+
+*/
+
+
+// See https://aka.ms/new-console-template for more information);
+
+Console.WriteLine("Pozdrav Svijetu šđčćšpčš");
+Console.WriteLine("Hello Osijek");
+
+
+
